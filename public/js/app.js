e1a6d3a03d71cc9bc425c29279c29df983136382
gepopp <office@sewemo.eu>
2019-04-25 22:42:40 +0200

    debug

--- public/js/app.js
+++ public/js/app.js
@@ -2202,6 +2202,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "MetaList",
@@ -39351,7 +39352,27 @@ var render = function() {
               staticClass: "list-group-item",
               attrs: { index: index }
             },
-            [_vm._v("\n            " + _vm._s(item.name) + "\n        ")]
+            [
+              _c("span", [
+                _c(
+                  "svg",
+                  {
+                    attrs: {
+                      xmlns: "http://www.w3.org/2000/svg",
+                      width: "24",
+                      height: "24",
+                      viewBox: "0 0 24 24"
+                    }
+                  },
+                  [
+                    _c("path", {
+                      attrs: { d: "M20 9H4v2h16V9zM4 15h16v-2H4v2z" }
+                    })
+                  ]
+                )
+              ]),
+              _vm._v("\n            " + _vm._s(item.name) + "\n        ")
+            ]
           )
         }),
         1
--- resources/js/components/MetaList.vue
+++ resources/js/components/MetaList.vue
@@ -2,9 +2,7 @@
     <div class="root">
         <SlickList v-model="items" class="list-group-flush" @input="updateList">
             <SlickItem v-for="(item, index) in items" :index="index" :key="index" class="list-group-item">
-                <span v-handle class="handle">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 9H4v2h16V9zM4 15h16v-2H4v2z"/></svg>
-                        </span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 9H4v2h16V9zM4 15h16v-2H4v2z"/></svg></span>
                 {{ item.name }}
             </SlickItem>
         </SlickList>
