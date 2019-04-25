d5c5e891ce553972aa01159240ac1a69ece3f2a6
gepopp <office@sewemo.eu>
2019-04-25 22:41:07 +0200

    debug meta sort

--- resources/js/components/MetaList.vue
+++ resources/js/components/MetaList.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="root">
         <SlickList v-model="items" class="list-group-flush" @input="updateList">
-            <SlickItem  v-for="(item, index) in items" :index="index" :key="index" class="list-group-item">
+            <SlickItem v-for="(item, index) in items" :index="index" :key="index" class="list-group-item">
+                <span v-handle class="handle">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 9H4v2h16V9zM4 15h16v-2H4v2z"/></svg>
+                        </span>
                 {{ item.name }}
             </SlickItem>
         </SlickList>
@@ -9,7 +12,7 @@
 </template>
 
 <script>
-    import { SlickList, SlickItem } from 'vue-slicksort';
+    import {SlickList, SlickItem} from 'vue-slicksort';
 
     export default {
         name: "MetaList",
@@ -18,13 +21,13 @@
             SlickItem,
             SlickList
         },
-        data(){
+        data() {
             return {
                 items: JSON.parse(this.metas),
             };
         },
-        methods:{
-            updateList(list){
+        methods: {
+            updateList(list) {
                 axios({
                     url: '/api/metasort',
                     method: 'POST',
