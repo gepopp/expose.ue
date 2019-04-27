03ad7e080f4cfc27757e8bdfe5f8b572dd01c89e
gepopp <office@sewemo.eu>
2019-04-27 18:23:44 +0200

    Template update

--- app/pdf/LocationPage.php
+++ app/pdf/LocationPage.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Storage;
 use Image;
 use App\pdf\helper\PolylineEncoder;
 
-class LocationPage
+class   LocationPage
 {
 
     public function content(BasePdf $pdf, RealEstate $realEstate,  $pageIds = [])
@@ -71,7 +71,7 @@ class LocationPage
         $pdf->SetXY(12, 30);
         $pdf->SetFont('helvetica', null, 12);
         $pdf->SetDrawColor(80, 80, 80);
-        $pdf->MultiCell(130, 4, $realEstateLocation->description, 0, 'J', 0, 2, '', 30, true, 0, true, true, null, 'T');
+        $pdf->MultiCell(130, 4, $realEstateLocation->description, 0, 'L', 0, 2, 12, 30, true, 0, true, true, null, 'T');
 
     }
 
